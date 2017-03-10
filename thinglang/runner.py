1fed9f26010f24af14abff9444862ed0861adb63
Yotam Tanay <yotam@paticon.com>
2017-03-10 14:38:36 +0200

    Add simplification between parsing and execution

--- thinglang/runner.py
+++ thinglang/runner.py
@@ -1,16 +1,18 @@
 from thinglang.execution.execution import ExecutionEngine
 from thinglang.lexer.lexer import lexer
 from thinglang.parser.parser import parse
+from thinglang.parser.simplifier import simplify
 
 
 def run(source):
     if not source:
-        raise ValueError('Got empty source')
+        raise ValueError('Source cannot be empty')
 
     source = source.strip().replace(' ' * 4, '\t')
 
     lexical_groups = list(lexer(source))
-    root_node = parse(lexical_groups)
+    tree = parse(lexical_groups)
+    root_node = simplify(tree)
 
     with ExecutionEngine(root_node) as engine:
         engine.execute()
