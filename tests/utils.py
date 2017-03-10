24831d3587209007f8ad25dfa64d160dc41907ad
Yotam Tanay <yotam@paticon.com>
2017-03-10 14:34:37 +0200

    Pull out utility functions

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,16 @@
 INDENT = '\n' + ' ' * 8
 
+
 def generate_simple_output_program(source):
     return """
 thing Program
     does start{source}
     """.format(source=INDENT + INDENT.join([source] if isinstance(source, str) else source))
+
+
+def generate_test_case_structure(dct):
+    lst = []
+    for name, groups in list(dct.items()):
+        for idx, group in enumerate(groups):
+            lst.append(('{} #{}'.format(name, idx + 1), group[0], group[1]))
+    return lst
