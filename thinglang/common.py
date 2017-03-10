aac1af1a3ed0c1276e8ad1040459ac56553857f0
Yotam Tanay <yotam@paticon.com>
2017-03-10 14:35:24 +0200

    New parsing context container

--- thinglang/common.py
+++ thinglang/common.py
@@ -25,3 +25,11 @@ class ResolvableValue(ValueType):
 class ObtainableValue(ValueType):
     pass
 
+
+class TokenContext(object):
+
+    def __init__(self, line, number):
+        self.line, self.number = line.strip(), number + 1
+
+    def __str__(self):
+        return '<L{}> {}'.format(self.number, self.line)
