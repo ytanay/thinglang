38d414286ad31551c2f5e3944d41d5e828781082
Yotam Tanay <yotam@paticon.com>
2017-03-17 16:23:00 +0200

    Added parsed conditional class

--- thinglang/parser/tokens.py
+++ thinglang/parser/tokens.py
@@ -163,6 +163,25 @@ class AssignmentOperation(BaseToken):
         return '{} {} = {}'.format(self.type, self.name, self.value)
 
 
+class Conditional(BaseToken):
+
+    ADVANCE = False
+    COMPARATORS = {
+        LexicalEquality: lambda lhs, rhs: lhs == rhs
+    }
+
+    def __init__(self, slice):
+        super(Conditional, self).__init__(slice)
+        _, self.lhs, self.comparator, self.rhs = slice
+        self.comparator = self.COMPARATORS[type(self.comparator)]
+
+    def describe(self):
+        return 'if {} {} {}'.format(self.lhs, self.comparator, self.rhs)
+
+    def evaluate(self, stack):
+        return self.comparator(self.lhs.evaluate(stack), self.rhs.evaluate(stack))
+
+
 class ReturnStatement(DefinitionPairToken):
     def __init__(self, slice):
         super().__init__(slice)
