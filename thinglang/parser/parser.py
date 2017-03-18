5d4cdab38d8b50c23c99c3309d9cfd4f14329067
Yotam Tanay <yotam@paticon.com>
2017-03-18 11:58:44 +0200

    Sort parse patterns again to prevent conflciting reductions

--- thinglang/parser/parser.py
+++ thinglang/parser/parser.py
@@ -1,3 +1,6 @@
+import copy
+import pprint
+
 from thinglang.common import ValueType
 from thinglang.lexer.symbols import LexicalGroupEnd
 from thinglang.lexer.symbols.arithmetic import FirstOrderLexicalBinaryOperation, SecondOrderLexicalBinaryOperation
@@ -10,30 +13,48 @@ from thinglang.parser.tokens import RootToken
 from thinglang.parser.tokens.arithmetic import ArithmeticOperation
 from thinglang.parser.tokens.base import AssignmentOperation
 from thinglang.parser.tokens.classes import ThingDefinition, MethodDefinition
-from thinglang.parser.tokens.functions import Access, ArgumentListPartial, ArgumentList, MethodCall, ReturnStatement
+from thinglang.parser.tokens.functions import Access, ArgumentListPartial, ArgumentList, MethodCall, ReturnStatement, \
+    ArgumentListDecelerationPartial
 from thinglang.parser.tokens.logic import Conditional
 
 PATTERNS = [
-    ((LexicalArgumentListIndicator, ValueType), ArgumentListDecelerationPartial),  # with a
-    ((LexicalIdentifier, LexicalAccess, LexicalIdentifier), Access),  # person.name
-    ((Access, ArgumentList), MethodCall),  # person.walk(...)
-    ((ValueType, SecondOrderLexicalBinaryOperation, ValueType), ArithmeticOperation),  # 4 * 2
-    ((ValueType, FirstOrderLexicalBinaryOperation, ValueType), ArithmeticOperation),  # 4 + 2
-    ((LexicalArgumentListIndicator, ValueType), ArgumentListPartial),  # with 2
-    ((LexicalParenthesesOpen, ValueType), ArgumentListPartial),  # (2
-    ((ArgumentListDecelerationPartial, LexicalSeparator, ValueType), ArgumentListDecelerationPartial),  # (2, 3
-    ((ArgumentListPartial, LexicalSeparator, ValueType), ArgumentListPartial),  # (2, 3
-    ((LexicalParenthesesOpen, LexicalParenthesesClose), ArgumentList),  # ()
-    ((ArgumentListPartial, LexicalParenthesesClose), ArgumentList),  # (2, 3)
-    ((ArgumentListPartial, LexicalGroupEnd), ArgumentList),  # (2, 3)
-    ((ArgumentListDecelerationPartial, LexicalGroupEnd), ArgumentList),  # (2, 3)
-    ((LexicalIdentifier, LexicalIdentifier, LexicalAssignment, ValueType), AssignmentOperation),  # number n = 1
-    ((LexicalIdentifier, LexicalAssignment, ValueType), AssignmentOperation),  # n = 2,
+
     ((LexicalDeclarationThing, LexicalIdentifier), ThingDefinition),  # thing Program
     ((LexicalDeclarationMethod, LexicalIdentifier, LexicalGroupEnd), MethodDefinition),  # does start
-    ((LexicalDeclarationMethod, LexicalIdentifier, ArgumentList), MethodDefinition),
-    ((LexicalReturnStatement, ValueType), ReturnStatement),
-    ((LexicalConditional, ValueType, LexicalComparison, ValueType), Conditional)
+    ((LexicalDeclarationMethod, LexicalIdentifier, ArgumentList), MethodDefinition),  # does start with a, b
+
+    ((LexicalArgumentListIndicator, ValueType), ArgumentListDecelerationPartial),  # with a
+
+    ((LexicalIdentifier, LexicalAccess, LexicalIdentifier), Access),  # person.name
+
+
+    ((ValueType, SecondOrderLexicalBinaryOperation, ValueType), ArithmeticOperation),  # 4 * 2
+    ((ValueType, FirstOrderLexicalBinaryOperation, ValueType), ArithmeticOperation),  # 4 + 2
+
+    ((ArgumentListPartial, SecondOrderLexicalBinaryOperation, ValueType), ArgumentListPartial),  # (4 * 2
+    ((ArgumentListPartial, FirstOrderLexicalBinaryOperation, ValueType), ArgumentListPartial),  # (4 + 2
+
+    ((LexicalParenthesesOpen, LexicalParenthesesClose), ArgumentList),  # ()
+
+    ((LexicalParenthesesOpen, ValueType), ArgumentListPartial),  # (2
+
+
+    ((ArgumentListDecelerationPartial, LexicalSeparator, ValueType), ArgumentListDecelerationPartial),  # (2, 3
+    ((ArgumentListPartial, LexicalSeparator, ValueType), ArgumentListPartial),  # (2, 3
+
+    ((ArgumentListPartial, LexicalParenthesesClose), ArgumentList),  # (2, 3)
+    ((ArgumentListDecelerationPartial, LexicalGroupEnd), ArgumentList),  # (2, 3)
+
+
+
+    ((LexicalConditional, ValueType, LexicalComparison, ValueType), Conditional),  # if x eq y
+
+    ((LexicalReturnStatement, ValueType), ReturnStatement),  # return 2
+
+    ((Access, ArgumentList), MethodCall),  # person.walk(...)
+
+    ((LexicalIdentifier, LexicalIdentifier, LexicalAssignment, ValueType), AssignmentOperation),  # number n = 1
+    ((LexicalIdentifier, LexicalAssignment, ValueType), AssignmentOperation),  # n = 2,
 
 ]
 
