ffe29487fcd34ad17569e3c5a9b6890d01cc8682
Yotam Tanay <yotam@paticon.com>
2017-03-18 12:09:19 +0200

    Cleanup simplifier methods and fix contextification

--- thinglang/parser/simplifier.py
+++ thinglang/parser/simplifier.py
@@ -1,8 +1,10 @@
 from thinglang.common import ObtainableValue
 from thinglang.lexer.symbols.base import LexicalIdentifier
+from thinglang.parser.tokens import Transient
 from thinglang.parser.tokens.arithmetic import ArithmeticOperation
 from thinglang.parser.tokens.base import AssignmentOperation
 from thinglang.parser.tokens.functions import MethodCall, ReturnStatement
+
 
 
 def simplify(tree):
@@ -19,10 +21,11 @@ def unwrap_returns(tree):
     had_change = False
 
     for child in set(tree.find(lambda x: isinstance(x, ReturnStatement) and isinstance(x.value, ObtainableValue))):
+        id, assignment = create_transient(child.value, child)
         siblings = child.parent.children
         idx = siblings.index(child)
-        local_id = LexicalIdentifier(next(ids)).contextify(child.context)
-        siblings[idx:idx + 1] = [AssignmentOperation([None, local_id, None, child.value]).contextify(child.parent), ReturnStatement([None, local_id]).contextify(child.parent)]
+
+        siblings[idx:idx + 1] = [assignment, ReturnStatement([None, id]).contextify(child.parent)]
         had_change = True
 
     return had_change
@@ -44,22 +47,6 @@ def reduce_method_calls(method_call, node, parent_call=None):
         parent_call.replace_argument(method_call, id)
 
 
-class Transient(object):
-    def __init__(self, idx):
-        self.idx = idx
-
-    def __str__(self):
-        return 'Transient({})'.format(self.idx)
-
-
-def id_generator():
-    counter = 0
-    while True:
-        yield Transient(counter)
-        counter += 1
-
-ids = id_generator()
-
 def is_compound(node):
     if not node:
         return False
@@ -68,7 +55,7 @@ def is_compound(node):
 
 
 def create_transient(value, parent):
-    local_id = LexicalIdentifier(next(ids))
+    local_id = LexicalIdentifier(Transient()).contextify(parent.context)
     return local_id, AssignmentOperation([None, local_id, None, value]).contextify(parent.parent)
 
 
