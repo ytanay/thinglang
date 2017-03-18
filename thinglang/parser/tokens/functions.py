38d47162f8ff46de2e31056a999b2aa096da0005
Yotam Tanay <yotam@paticon.com>
2017-03-18 11:55:21 +0200

    Added arithmetic compoisiton support to argument lists

--- thinglang/parser/tokens/functions.py
+++ thinglang/parser/tokens/functions.py
@@ -1,6 +1,8 @@
-from thinglang.common import ObtainableValue
+from thinglang.common import ObtainableValue, ValueType
+from thinglang.lexer.symbols import LexicalBinaryOperation
 from thinglang.lexer.symbols.base import LexicalIdentifier
 from thinglang.parser.tokens import BaseToken, DefinitionPairToken
+from thinglang.parser.tokens.arithmetic import ArithmeticOperation
 
 
 class Access(BaseToken):
@@ -11,13 +13,23 @@ class Access(BaseToken):
     def describe(self):
         return '.'.join(self.target)
 
+
 class ArgumentListPartial(BaseToken):
     def __init__(self, slice):
         super(ArgumentListPartial, self).__init__(slice)
-        if isinstance(slice[0], ArgumentListPartial):
+        if len(slice) == 3 and isinstance(slice[1], LexicalBinaryOperation):
+            self.value = slice[0].value[:-1] + [ArithmeticOperation([slice[0][-1]] + slice[1:])]
+        elif isinstance(slice[0], ArgumentListPartial):
             self.value = slice[0].value + [slice[2]]
         else:
             self.value = [slice[1]]
+
+    def __len__(self):
+        return len(self.value)
+
+    def __getitem__(self, item):
+        return self.value[item]
+
 
 
 class ArgumentList(BaseToken):
