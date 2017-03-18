2e397bc61ed3bb169c826e02ea01ebaf7363b5bf
Yotam Tanay <yotam@paticon.com>
2017-03-18 11:54:17 +0200

    Added AST sanity validation before and after simplification

--- thinglang/runner.py
+++ thinglang/runner.py
@@ -12,16 +12,16 @@ def run(source):
 
     lexical_groups = list(lexer(source))
     tree = parse(lexical_groups)
-    validate(tree, None)
+    validate(tree)
     root_node = simplify(tree)
+    validate(tree)
 
     with ExecutionEngine(root_node) as engine:
         engine.execute()
         return engine.results()
 
 
-def validate(node, parent):
-    assert node.parent is parent
-    print('validating parent on {} = {}'.format(node, parent))
+def validate(node, parent=None):
+    assert node.parent is parent, 'expected node {} to have parent {} but got {}'.format(node, parent, node.parent)
     for child in node.children:
         validate(child, node)
