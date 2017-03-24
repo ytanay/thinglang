b66abbf66fdc614515c4cedb94fbbee8b9063b9d
Yotam Tanay <yotam@paticon.com>
2017-03-24 15:05:19 +0300

    Temporarily disabling new test infra

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,7 +1,10 @@
+import pytest
+
 from thinglang import utils
 from thinglang.runner import run
 
 
+@pytest.mark.skip
 def test_external_file(source):
     utils.print_header('Expected output', source.expected_output)
     assert run(source.source).output == source.expected_output
