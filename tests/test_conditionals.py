18aafd9218efe636c6efb75980b2014d43b6736e
Yotam Tanay <yotam@paticon.com>
2017-03-24 10:48:27 +0300

    Test for unconditional else branches

--- tests/test_conditionals.py
+++ tests/test_conditionals.py
@@ -12,3 +12,20 @@ thing Program
         if "dog" eq "cat"
             Output.write("dog is cat")
     """).output == """dog is dog""".strip()
+
+
+def test_unconditional_else():
+    assert run("""
+thing Program
+    does start
+        if "dog" eq "dog"
+            Output.write("dog is dog")
+        otherwise
+            Output.write("dog is not dog")
+
+        if "dog" eq "cat"
+            Output.write("dog is cat")
+        otherwise
+            Output.write("dog is not cat")
+    """).output == """dog is dog\ndog is not cat""".strip()
+
