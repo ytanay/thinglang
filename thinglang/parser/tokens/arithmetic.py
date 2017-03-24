3319b68d25fa964701b60eb5f771f6eb43542313
Yotam Tanay <yotam@paticon.com>
2017-03-24 10:51:54 +0300

    Genercify arithemtic operation to support logical operations + use new explicit lexical operator types

--- thinglang/parser/tokens/arithmetic.py
+++ thinglang/parser/tokens/arithmetic.py
@@ -1,22 +1,26 @@
 from thinglang.common import ValueType
+from thinglang.lexer.symbols.arithmetic import LexicalAddition, LexicalMultiplication, LexicalSubtraction, \
+    LexicalDivision
+from thinglang.lexer.symbols.logic import LexicalEquality
 from thinglang.parser.tokens import BaseToken
 
 
 class ArithmeticOperation(BaseToken, ValueType):
     OPERATIONS = {
-        "+": lambda rhs, lhs: rhs + lhs,
-        "*": lambda rhs, lhs: rhs * lhs,
-        "-": lambda rhs, lhs: rhs - lhs,
-        "/": lambda rhs, lhs: rhs / lhs
+        LexicalAddition: lambda rhs, lhs: rhs + lhs,
+        LexicalMultiplication: lambda rhs, lhs: rhs * lhs,
+        LexicalSubtraction: lambda rhs, lhs: rhs - lhs,
+        LexicalDivision: lambda rhs, lhs: rhs / lhs,
+        LexicalEquality: lambda rhs, lhs: lhs == rhs
     }
 
     def __init__(self, slice):
         super(ArithmeticOperation, self).__init__(slice)
         self.arguments = [slice[0], slice[2]]
-        self.operator = slice[1]
+        self.operator = type(slice[1])
 
     def evaluate(self, stack):
-        return self.OPERATIONS[self.operator.operator](self[0].evaluate(stack), self[1].evaluate(stack))
+        return self.OPERATIONS[self.operator](self[0].evaluate(stack), self[1].evaluate(stack))
 
     def describe(self):
         return '|{} {} {}|'.format(self[0], self.operator, self[1])
