21d940192fa390b1a2de3183e099194bceaeeafe
Yotam Tanay <yotam@paticon.com>
2017-03-25 19:20:19 +0300

    Add test for more complex array initization case

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -10,3 +10,25 @@ thing Program
 
     """).output == """['yotam', 'andrew', 'john']"""
 
+
+def test_array_initialization_over_function_calls():
+    assert run("""
+thing Program
+    does start
+        array numbers = self.build_array()
+        Output.write(numbers)
+
+    does get_10
+        return 10
+
+    does get_7
+        return 7
+
+    does add with a, b
+        return a + b
+
+    does build_array
+        return [self.get_7(), self.get_10(), self.add(9, self.get_7() + self.get_10())]
+
+    """).output == """[7, 10, 26]"""
+
