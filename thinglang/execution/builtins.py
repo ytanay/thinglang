14073ba2cf3f09a7ed15de1274d8b3f69d9b742b
Yotam Tanay <yotam@paticon.com>
2017-03-25 13:57:08 +0300

    Brought input and output builtins inline

--- thinglang/execution/builtins.py
+++ thinglang/execution/builtins.py
@@ -1,23 +1,21 @@
 class ThingObjectBase(object):
 
     def __getitem__(self, item):
-
-    def __contains__(self, item):
-        return hasattr(self, item)
-
-
-
-    def __init__(self):
-        self.data = ""
-
-    def write(self, args):
-        self.data += ' '.join(str(x) for x in args) + "\n"
-
-    def __getitem__(self, item):
         return getattr(self, item)
 
     def __contains__(self, item):
         return hasattr(self, item)
+
+
+class ThingObjectOutput(ThingObjectBase):
+
+    def __init__(self):
+        self.data = []
+
+    def write(self, *args):
+        self.data.append(' '.join(str(x) for x in args))
+
+
 class ThingObjectInput(ThingObjectBase):
 
     def __init__(self):
