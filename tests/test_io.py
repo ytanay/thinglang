89f1d3a37a2ee87748d98ce03f7bffd9e1e5d446
Yotam Tanay <yotam@paticon.com>
2017-03-25 13:59:14 +0300

    Added test for multiple lines of input

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,3 +7,14 @@ thing Program
         Output.write("Input is:", input)
         """).output == "Input is: single input line"
 
+
+def test_input_multiple_lines():
+    with patch('sys.stdin', io.StringIO('first line\nsecond line')):
+        assert run("""
+thing Program
+    does start
+        text input = Input.get_line()
+        Output.write("1:", input)
+        Output.write("2:", Input.get_line())
+        """).output == "1: first line\n2: second line"
+
