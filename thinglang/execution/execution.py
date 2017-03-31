28d53126823d206af4053bb7b382bb717d156465
Yotam Tanay <yotam@paticon.com>
2017-03-31 23:11:58 +0300

    Fix assignment of result of class constructor

--- thinglang/execution/execution.py
+++ thinglang/execution/execution.py
@@ -76,7 +76,11 @@ class ExecutionEngine(object):
                 last_frame = self.stack.pop()
                 utils.log('Stack frame termination, with return value {}, binding to {}'.format(last_frame.return_value, target.target_arg))
 
-                if target.target_arg is not None:
+                # TODO: verify no returns in constructor
+
+                if target.constructor:
+                    self.stack[target.target_arg] = last_frame.instance
+                elif target.target_arg is not None:
                     self.stack[target.target_arg] = last_frame.return_value
                 continue
 
