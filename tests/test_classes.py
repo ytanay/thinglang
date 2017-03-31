7d2e6e0d78e71f6bd772f39070ff00022c9b6097
Yotam Tanay <yotam@paticon.com>
2017-03-31 18:02:10 +0300

    Integration test for classes

---
+++ tests/test_classes.py
@@ -0,0 +1,34 @@
+from thinglang import run
+
+
+def test_class_integration():
+    assert run("""
+thing Person
+    has text name
+    has number age
+
+    created with name
+        self.name = name
+        self.age = 0
+
+    does grow_up
+        self.age = self.age + 1
+
+    does say_hello with excitement_level
+        Output.write("Hello from", self.name, ", who's ", self.age, "and is always up for a fun game of tag.")
+
+
+thing Program
+    does start
+        text name = "yotam"
+        text wants_to_grow_up = true
+        #text name = Input.get_line("What is your name?")
+        #text wants_to_grow_up = Input.get_line("Do you want to grow up?")
+        Person person = create Person(name)
+
+        if wants_to_grow_up
+            person.grow_up()
+
+        person.say_hello()
+    """).output == """dog is dog"""
+
