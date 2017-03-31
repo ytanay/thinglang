3c64002217795e5d8d3eebb7b06f8ad72f342564
Yotam Tanay <yotam@paticon.com>
2017-03-31 23:15:54 +0300

    Add proper support for constructor calls to MethodCall

--- thinglang/parser/tokens/functions.py
+++ thinglang/parser/tokens/functions.py
@@ -1,4 +1,5 @@
-from thinglang.lexer.symbols.base import  LexicalAccess
+from thinglang.lexer.symbols.base import  LexicalAccess, LexicalIdentifier
+from thinglang.lexer.symbols.functions import LexicalClassInitialization
 from thinglang.parser.tokens import BaseToken, DefinitionPairToken
 from thinglang.parser.tokens.collections import ListInitializationPartial, ListInitialization
 from thinglang.utils.type_descriptors import ValueType
@@ -9,6 +10,9 @@ class Access(BaseToken):
         super(Access, self).__init__(slice)
         self.target = [x for x in slice if not isinstance(x, LexicalAccess)]
 
+    def evaluate(self, resolver):
+        return resolver.resolve(self)
+
     def describe(self):
         return '.'.join(str(x) for x in self.target)
 
@@ -18,7 +22,6 @@ class Access(BaseToken):
 
 class ArgumentListPartial(ListInitializationPartial):
     pass
-
 
 class ArgumentListDecelerationPartial(ArgumentListPartial):
     pass
@@ -31,8 +34,16 @@ class ArgumentList(ListInitialization):
 class MethodCall(BaseToken, ValueType):
     def __init__(self, slice):
         super(MethodCall, self).__init__(slice)
-        self.target, self.arguments = slice
         self.value = self
+
+        if isinstance(slice[0], LexicalClassInitialization):
+            self.target = Access([slice[1], LexicalIdentifier.constructor().contextify(slice[0])])
+            self.arguments = slice[2]
+            self.constructing_call = True
+        else:
+            self.target, self.arguments = slice
+            self.constructing_call = False
+
 
         if not self.arguments:
             self.arguments = ArgumentList()
