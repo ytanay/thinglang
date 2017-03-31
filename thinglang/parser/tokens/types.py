1248dbc10b3ff578edc25b81d8f694ffef5d1d3a
Yotam Tanay <yotam@paticon.com>
2017-03-31 23:14:41 +0300

    Implement CastOperation token

--- thinglang/parser/tokens/types.py
+++ thinglang/parser/tokens/types.py
@@ -1,5 +1,7 @@
+from thinglang.lexer.symbols.base import LexicalIdentifier
+from thinglang.parser.tokens import BaseToken
 from thinglang.parser.tokens.collections import ListInitializationPartial, ListInitialization
-from thinglang.utils.type_descriptors import ValueType
+from thinglang.utils.type_descriptors import ValueType, ReplaceableArguments
 
 
 class ArrayInitializationPartial(ListInitializationPartial):
@@ -8,3 +10,18 @@ class ArrayInitializationPartial(ListInitializationPartial):
 
 class ArrayInitialization(ListInitialization, ValueType):
     pass
+
+
+class CastOperation(BaseToken, ValueType, ReplaceableArguments):
+
+    CASTERS = {
+        LexicalIdentifier("number"): lambda x: int(x)
+    }
+
+    def __init__(self, slice):
+        super(CastOperation, self).__init__(slice)
+        self.arguments = [slice[0]]
+        self.caster = self.CASTERS[slice[2]]
+
+    def evaluate(self, resolver):
+        return self.caster(self.arguments[0].evaluate(resolver))
