cfdc38c3b4da413baac33fcb2eace40dc2026917
Yotam Tanay <yotam@paticon.com>
2017-03-31 14:27:06 +0300

    Expanded concept of MethodDefnition to cover generic constrcutor cases

--- thinglang/parser/tokens/classes.py
+++ thinglang/parser/tokens/classes.py
@@ -1,19 +1,41 @@
-from thinglang.parser.tokens import DefinitionPairToken
+from thinglang.lexer.symbols.functions import LexicalDeclarationConstructor
+from thinglang.parser.tokens import DefinitionPairToken, BaseToken
 from thinglang.parser.tokens.functions import ArgumentList
 
 
 class ThingDefinition(DefinitionPairToken):
-    pass
+
+    def __contains__(self, item):
+        return any(child.value == item for child in self.children)
+
+    def __getitem__(self, item):
+        return [child for child in self.children if child.value == item][0]
 
 
 class MethodDefinition(DefinitionPairToken):
     def __init__(self, slice):
         super(MethodDefinition, self).__init__(slice)
 
-        if isinstance(slice[2], ArgumentList):
-            self.arguments = slice[2]
+        if isinstance(slice[0], LexicalDeclarationConstructor):
+            self.value = "constructor"
+            argument_list = slice[1]
         else:
-            self.arguments = []
+            argument_list = slice[2]
+
+        if isinstance(argument_list, ArgumentList):
+            self.arguments = argument_list
+        else:
+            self.arguments = ArgumentList()
 
     def describe(self):
         return '{}, args={}'.format(self.value, self.arguments)
+
+
+class MemberDefinition(BaseToken):
+    def __init__(self, slice):
+        super(MemberDefinition, self).__init__(slice)
+
+        _, self.type, self.name = slice
+
+    def describe(self):
+        return 'has {} {}'.format(self.type, self.name)
