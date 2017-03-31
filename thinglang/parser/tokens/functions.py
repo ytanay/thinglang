eae679d8d8cd150fde543a7be62c4408806a78c7
Yotam Tanay <yotam@paticon.com>
2017-03-31 14:43:13 +0300

    Updated ArgumentList/Partial to user generic list initialization tokens

--- thinglang/parser/tokens/functions.py
+++ thinglang/parser/tokens/functions.py
@@ -1,7 +1,6 @@
-from thinglang.lexer.symbols import LexicalBinaryOperation
 from thinglang.lexer.symbols.base import LexicalIdentifier
 from thinglang.parser.tokens import BaseToken, DefinitionPairToken
-from thinglang.parser.tokens.arithmetic import ArithmeticOperation
+from thinglang.parser.tokens.collections import ListInitializationPartial, ListInitialization
 from thinglang.utils.type_descriptors import ValueType
 
 
@@ -14,50 +13,16 @@ class Access(BaseToken):
         return '.'.join(self.target)
 
 
-class ArgumentListPartial(BaseToken):
-    def __init__(self, slice):
-        super(ArgumentListPartial, self).__init__(slice)
-        if len(slice) == 3 and isinstance(slice[1], LexicalBinaryOperation):
-            self.value = slice[0].value[:-1] + [ArithmeticOperation([slice[0][-1]] + slice[1:])]
-        elif isinstance(slice[0], ArgumentListPartial):
-            self.value = slice[0].value + [slice[2]]
-        else:
-            self.value = [slice[1]]
-
-    def __len__(self):
-        return len(self.value)
-
-    def __getitem__(self, item):
-        return self.value[item]
+class ArgumentListPartial(ListInitializationPartial):
+    pass
 
 
 class ArgumentListDecelerationPartial(ArgumentListPartial):
     pass
 
 
-class ArgumentList(BaseToken):
-    def __init__(self, slice=None):
-        super(ArgumentList, self).__init__(slice)
-
-        if slice and isinstance(slice[0], ArgumentListPartial):
-            self.arguments = slice[0].value
-        else:
-            self.arguments = []
-
-    def __iter__(self):
-        return iter(self.arguments)
-
-    def __len__(self):
-        return len(self.arguments)
-
-    def evaluate(self, stack):
-        return [value.evaluate(stack) for value in self.arguments]
-
-    def describe(self):
-        return self.arguments
-
-    def replace(self, original, replacement):
-        self.arguments = [replacement if x is original else x for x in self.arguments]
+class ArgumentList(ListInitialization):
+    pass
 
 
 class MethodCall(BaseToken, ValueType):
