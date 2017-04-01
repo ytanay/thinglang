7c4108a7520d23afd28a8037ce64e20a202e88cc
Yotam Tanay <yotam@paticon.com>
2017-04-01 19:46:53 +0300

    Slightly functional analysis for method call resolution and argument matching

--- thinglang/parser/analyzer.py
+++ thinglang/parser/analyzer.py
@@ -19,6 +19,19 @@ def verify_thing_definitions(ast):
             raise EmptyThingDefinition()
 
 
+def analyze_method_resolution(ast):
+    for child in ast.find(MethodCall):
+        if child.target[0].is_self():
+            thing_definition = child.upwards(ThingDefinition)
+        else:
+            thing_definition = ast.find(ThingDefinition, lambda x: x.name == child.target[0], single=True)
+
+        if thing_definition is None:
+            print('Warning: method resolution cannot be validated!')
+        elif len(child.arguments) != len(thing_definition.find(MethodDefinition, lambda x: x.name == child.target[1], single=True).arguments):
+            raise ArgumentCountMismatch()
+
+
 def analyze(ast):
     analyze_method_resolution(ast)
     verify_method_definitions(ast)
