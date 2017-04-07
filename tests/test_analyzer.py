b41c91da610419c3a1cc7031fd8554cee8be3a6c
Yotam Tanay <yotam@paticon.com>
2017-04-07 17:04:28 +0300

    Add integration test for variable resolution

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -38,6 +38,57 @@ thing Program
     """)
 
 
+
+def test_undefined_variable_exceptions():
+    with pytest.raises(ParseErrors) as error:
+        run("""
+thing Program
+    setup
+        text out1 = "value of ou1"
+        Output.write(out1, out2, out3)
+
+        if some_condition
+            text in1 = "value of in1"
+            Output.write(out1, in1, out3)
+        otherwise if operand1 + operand2 < operand3
+            Output.write(out1, in1)
+
+        repeat while out1 > in1
+            Output.write(out1, in1, in2)
+
+        Output.write(out1, out2, in1)
+
+    does real_thing
+        Output.write("at Program:real_thing")
+
+
+thing ExternalClass
+    does real_thing
+        Output.write("at real_thing")
+    """)
+
+    assert error.value.args == (
+        UnresolvedReference(LexicalIdentifier("out2")),  # Outer statement
+        UnresolvedReference(LexicalIdentifier("out3")),
+
+        UnresolvedReference(LexicalIdentifier("some_condition")),  # Conditional block
+
+        UnresolvedReference(LexicalIdentifier("out3")),  # Inner statement
+
+        UnresolvedReference(LexicalIdentifier("operand1")),  # Second conditional
+        UnresolvedReference(LexicalIdentifier("operand2")),
+        UnresolvedReference(LexicalIdentifier("operand3")),
+
+        UnresolvedReference(LexicalIdentifier("in1")),  # Inner statement
+
+        UnresolvedReference(LexicalIdentifier("in1")),  # Loop
+
+        UnresolvedReference(LexicalIdentifier("in1")),  # Inner statement
+        UnresolvedReference(LexicalIdentifier("in2")),
+
+        UnresolvedReference(LexicalIdentifier("out2")),  # Outer statement
+        UnresolvedReference(LexicalIdentifier("in1")),
+
 @pytest.mark.parametrize('line', ['', '1', '1, 2, 3'])
 def test_argument_count_mismatch(line):
     with pytest.raises(ArgumentCountMismatch):
