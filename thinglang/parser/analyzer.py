4969715c4d7b5774e0be0a0438a1c8600ddfaa46
Yotam Tanay <yotam@paticon.com>
2017-04-07 17:36:57 +0300

    Extracted reference resoulution to inspection

--- thinglang/parser/analyzer.py
+++ thinglang/parser/analyzer.py
@@ -71,11 +71,6 @@ class Analysis(object):
 
         self.inspect(node)
 
-
-        if node.references():
-            for ref in (x for x in emit_recursively(node.references(), (Access, LexicalIdentifier)) if not self.validate_scoping(x)):
-                self.report_exception(UnresolvedReference(ref))
-
         if node.SCOPING:
             self.scoping.enter()
 
@@ -97,6 +92,11 @@ class Analysis(object):
     def report_exception(self, error):
         self.errors.append(error)
 
+    @inspects()
+    def verify_reference_dependencies(self, node):
+        if node.references():
+            for ref in (x for x in emit_recursively(node.references(), (Access, LexicalIdentifier)) if not self.validate_scoping(x)):
+                yield UnresolvedReference(ref)
 
     @inspects(AssignmentOperation)
     def mark_variable_deceleration(self, node):
