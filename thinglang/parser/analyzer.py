71680ac91a6a1d5abc283ecf150140adb1092cba
Yotam Tanay <yotam@paticon.com>
2017-04-07 17:02:58 +0300

    Implemented new "recursive" list traversal utility

--- thinglang/parser/analyzer.py
+++ thinglang/parser/analyzer.py
@@ -4,11 +4,10 @@ from thinglang.execution.resolver import Resolver
 from thinglang.execution.stack import Frame
 from thinglang.lexer.tokens.base import LexicalIdentifier
 from thinglang.parser.errors import ParseErrors
-from thinglang.parser.symbols import BaseSymbol
 from thinglang.parser.symbols.base import AssignmentOperation
 from thinglang.parser.symbols.classes import MethodDefinition, ThingDefinition
 from thinglang.parser.symbols.functions import ReturnStatement, MethodCall, Access
-from thinglang.utils.collection_utils import flatten_list, flatten_recursively, emit_recursively
+from thinglang.utils.collection_utils import  emit_recursively
 
 
 def verify_method_definitions(ast):
--- thinglang/utils/collection_utils.py
+++ thinglang/utils/collection_utils.py
@@ -1,2 +1,18 @@
+import collections
+
+
 def flatten_list(l):
     return [item for sublist in l for item in sublist]
+
+
+def emit_recursively(iterable, expected_type):
+    targets = collections.deque([iterable])
+    while targets:
+        target = targets.popleft()
+        if isinstance(target, expected_type):
+            yield target
+            continue
+        try:
+            targets.extend(target)
+        except TypeError:
+            pass
