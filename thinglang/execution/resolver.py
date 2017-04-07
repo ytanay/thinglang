e6223ae92570771e0255b65299f98db620158214
Yotam Tanay <yotam@paticon.com>
2017-04-07 17:00:27 +0300

    Added option to resolve references without throwing an exception

--- thinglang/execution/resolver.py
+++ thinglang/execution/resolver.py
@@ -1,16 +1,21 @@
-from thinglang.execution.errors import UnknownVariable
+from thinglang.execution.errors import UnresolvedReference
 from thinglang.lexer.tokens.base import LexicalIdentifier
 from thinglang.parser.symbols.functions import Access
 
-UNRESOLVABLE_REFERENCE = object()
+
 
 
 class Resolver(object):
+
+    UNRESOLVED_REFERENCE = object()
 
     def __init__(self, stack, heap):
         self.stack, self.heap = stack, heap
 
     def resolve(self, identifier):
+        return self.validate(identifier, self.lookup(identifier))
+
+    def lookup(self, identifier):
         if isinstance(identifier, LexicalIdentifier):
             if identifier in self.stack:
                 return self.stack[identifier]
@@ -18,30 +23,30 @@ class Resolver(object):
             if identifier in self.heap:
                 return self.heap[identifier]
 
-            self.validate(identifier, UNRESOLVABLE_REFERENCE)
+            return self.UNRESOLVED_REFERENCE
 
         if identifier[0].is_self():
-            return self.validate(identifier, self.search(identifier[1:], self.stack.instance))
+            return self.search(identifier[1:], self.stack.instance)
 
         stack_value = self.search(identifier, self.stack)
-        if stack_value is not UNRESOLVABLE_REFERENCE:
+        if stack_value is not self.UNRESOLVED_REFERENCE:
             return stack_value
 
-        return self.validate(identifier, self.search(identifier, self.heap))
+        return self.search(identifier, self.heap)
 
-    @staticmethod
-    def search(identifier, context):
+    @classmethod
+    def search(cls, identifier, context):
         for component in identifier:
             if component not in context:
-                return UNRESOLVABLE_REFERENCE
+                return cls.UNRESOLVED_REFERENCE
 
             context = context[component]
 
         return context
 
     def validate(self, identifier, value):
-        if value is UNRESOLVABLE_REFERENCE:
-            raise UnknownVariable(f'Cannot find reference {identifier} in stack={self.stack}, heap={self.heap}')
+        if value is self.UNRESOLVED_REFERENCE:
+            raise UnresolvedReference(f'Cannot find reference {identifier} in stack={self.stack}, heap={self.heap}')
         return value
 
     def set(self, key, value):
