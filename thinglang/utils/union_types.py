9fcef585368bfa229973b7ddb6eda530a50d3d4f
Yotam Tanay <yotam@paticon.com>
2017-04-08 11:51:53 +0300

    New union type for potentially obtainbable values

--- thinglang/utils/union_types.py
+++ thinglang/utils/union_types.py
@@ -1,6 +1,9 @@
 from thinglang.lexer.tokens.base import LexicalIdentifier
-from thinglang.parser.symbols.functions import Access
+from thinglang.parser.symbols.arithmetic import ArithmeticOperation
+from thinglang.parser.symbols.functions import Access, MethodCall
+from thinglang.parser.symbols.types import ArrayInitialization, CastOperation
 from thinglang.utils.type_descriptors import ValueType
 
 ACCESS_TYPES = Access, LexicalIdentifier
 POTENTIALLY_RESOLVABLE = Access, LexicalIdentifier, ValueType
+POTENTIALLY_OBTAINABLE = MethodCall, ArithmeticOperation, ArrayInitialization, CastOperation
