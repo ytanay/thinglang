2bf9a0e4ada71cc8cbd445a4e77564b2b5531b39
Yotam Tanay <yotam@paticon.com>
2017-04-09 10:42:00 +0300

    Stupidly inject builtins after program source

--- thinglang/__init__.py
+++ thinglang/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from thinglang import utils
 from thinglang.execution.execution import ExecutionEngine
 from thinglang.lexer.lexer import lexer
@@ -5,12 +7,19 @@ from thinglang.parser.analyzer import Analyzer
 from thinglang.parser.parser import parse
 from thinglang.parser.simplifier import Simplifier
 
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'include')
+
+
+def collect_includes():
+    files = [os.path.join(BASE_DIR, path) for path in os.listdir(BASE_DIR)]
+    return '\n' + '\n'.join(open(f).read() for f in files)
+
 
 def run(source):
     if not source:
         raise ValueError('Source cannot be empty')
 
-    source = source.strip().replace(' ' * 4, '\t')
+    source = (source + collect_includes()).strip().replace(' ' * 4, '\t')
 
     utils.print_header('Source', source)
 
@@ -18,9 +27,10 @@ def run(source):
     ast = parse(lexical_groups)
 
     Simplifier(ast).run()
-    Analyzer(ast).run()
 
     utils.print_header('Parsed AST', ast.tree())
+
+    Analyzer(ast).run()
 
     with ExecutionEngine(ast) as engine:
         engine.execute()
