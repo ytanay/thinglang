e90e5b67ba35fdb84b8d56b4392e62391c75de81
Yotam Tanay <yotam@paticon.com>
2017-04-09 10:39:13 +0300

    Created RangeGenerator proxy over `create Range(start, end)` call

--- thinglang/parser/symbols/proxies.py
+++ thinglang/parser/symbols/proxies.py
@@ -1,5 +1,8 @@
+from thinglang.lexer.tokens.base import LexicalIdentifier
+from thinglang.parser.symbols import BaseSymbol
 from thinglang.parser.symbols.arithmetic import ArithmeticOperation
-from thinglang.parser.symbols.functions import ArgumentListPartial
+from thinglang.parser.symbols.functions import ArgumentListPartial, MethodCall, Access, ArgumentList
+from thinglang.utils.type_descriptors import ValueType
 
 
 class ConstrainedArithmeticOperation(object):
@@ -7,3 +10,11 @@ class ConstrainedArithmeticOperation(object):
     @classmethod
     def construct(cls, slice):
         return ArgumentListPartial([None, ArithmeticOperation([slice[0][0], slice[1], slice[2][0]])])
+
+
+class RangeGenerator(BaseSymbol, ValueType):
+
+    @classmethod
+    def construct(cls, slice):
+        print(slice)
+        return MethodCall.create_constructing_call(LexicalIdentifier('Range'), ArgumentList([slice[0], slice[-1]]))
