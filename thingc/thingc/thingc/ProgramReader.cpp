4fa567ff50e108a25c91832f83469012107ac92e
Yotam Tanay <yotam@paticon.com>
2017-05-13 13:38:47 +0300

    Class level iterative bytecode reader

--- thingc/thingc/thingc/ProgramReader.cpp
+++ thingc/thingc/thingc/ProgramReader.cpp
@@ -55,6 +55,42 @@ PThingInstance ProgramReader::read_data_block() {
 
 	}
 }
+
+std::vector<TypeInfo> ProgramReader::read_code() {
+	
+
+	std::cerr << "Reading program..." << std::endl;
+	std::vector<TypeInfo> user_types;
+
+	while (in_program()) {
+		auto class_definition_size = read_size();
+		std::cerr << "\tEncountered class (id=" << (user_types.size() - 1) << ") of size " << class_definition_size << std::endl;
+		user_types.push_back(read_class(class_definition_size));
+	}
+
+	if (index != program_size) {
+		throw std::exception(std::string("Index mismatch " + std::to_string(index) + ", " + std::to_string(program_size)).c_str());
+	}
+	else {
+		std::cerr << "Program processed successfully" << std::endl << std::endl;
+	}
+
+	return user_types;
+}
+
+
+TypeInfo ProgramReader::read_class(uint32_t size) {
+	uint32_t last_index = index + size;
+	std::vector<MethodDefinition> methods;
+
+	while (index < last_index) {
+		auto method_size = read_size();
+		methods.push_back(read_method(method_size));
+	}
+	return TypeInfo("Unknown class", "", methods);
+
+}
+
 MethodDefinition ProgramReader::read_method(size_t size) {
 	uint32_t frame_size = read_size();
 	uint32_t arguments = read_size();
