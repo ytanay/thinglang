7dcaf790d0aa56146345f94143b4c7342535f167
Yotam Tanay <yotam@paticon.com>
2017-05-13 13:34:24 +0300

    Static program member initialization

---
+++ thingc/thingc/thingc/Program.cpp
@@ -0,0 +1,8 @@
+#include "Program.h"
+#include "NoneType.h"
+
+std::stack<PThingInstance> Program::stack;
+std::vector<PThingInstance> Program::static_data;
+std::stack<Frame> Program::frames;
+PThingInstance Program::current_instance;
+std::vector<TypeInfo> Program::types = { NoneType() };
