bb47d7db349b6de54440b2298fb0c6377a4a7e0a
Yotam Tanay <yotam@paticon.com>
2017-05-13 13:36:44 +0300

    Implement + support in numbers

--- thingc/thingc/thingc/NumberInstance.h
+++ thingc/thingc/thingc/NumberInstance.h
@@ -15,6 +15,12 @@ public:
 		return std::to_string(val);
 	}
 
+	static void add() {
+		auto lhs = static_cast<NumberInstance*>(Program::pop().get());
+		auto rhs = static_cast<NumberInstance*>(Program::pop().get());
+		auto ptr = PThingInstance(new NumberInstance(lhs->val + rhs->val));
+		Program::push(ptr);
+	}
 
 	const int val;
 	static const std::vector<InternalMethod> methods;
