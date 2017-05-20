f3b2812917a934a84c2c4db1b402e6cb2ef22f43
Yotam Tanay <yotam@paticon.com>
2017-05-20 19:24:26 +0300

    Add optional secondary target argument

--- thingc/execution/Symbol.h
+++ thingc/execution/Symbol.h
@@ -9,10 +9,13 @@ class Symbol {
 public:
 	Symbol(Opcode opcode) : opcode(opcode) {};
 	Symbol(Opcode opcode, unsigned int target) : opcode(opcode), target(target) {};
+    Symbol(Opcode opcode, unsigned int target, unsigned int secondary) : opcode(opcode), target(target), secondary(secondary) {};
 
-	void execute();
+
+    void execute();
 
 private:
 	Opcode opcode;
-	unsigned int target;
+	unsigned int target = 0;
+    unsigned int secondary = 0;
 };
