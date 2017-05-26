697544de669813e950e34add359d2c4652d7c971
Yotam Tanay <yotam@paticon.com>
2017-05-26 15:58:01 +0300

    Reference indexing traversal engine

---
+++ thinglang/compiler/indexer.py
@@ -0,0 +1,65 @@
+from thinglang.compiler.allocation import LinearMemoryAllocationLayout
+from thinglang.compiler.references import ResolvedReference
+from thinglang.lexer.tokens.base import LexicalIdentifier
+from thinglang.parser.symbols.base import AssignmentOperation
+from thinglang.parser.symbols.classes import ThingDefinition, MethodDefinition
+from thinglang.parser.symbols.functions import MethodCall
+from thinglang.utils.tree_utils import TreeTraversal, inspects
+
+
+class Indexer(TreeTraversal):
+
+    def __init__(self, ast):
+        super().__init__(ast)
+        self.instance_members, self.locals, self.current_method = None, None, None
+
+    def run(self):
+        super(Indexer, self).run()
+        self.current_method.frame_size = self.locals.next_index
+
+    @inspects(ThingDefinition)
+    def set_thing_context(self, node: ThingDefinition) -> None:
+        self.instance_members = {
+            member.name: (idx, member) for idx, member in enumerate(node.members())
+        }
+
+    @inspects(MethodDefinition)
+    def set_method_context(self, node: MethodDefinition) -> None:
+        if self.current_method:
+            self.current_method.frame_size = self.locals.next_index
+
+        self.current_method = node
+        self.locals = LinearMemoryAllocationLayout({
+            argument: idx for idx, argument in enumerate(node.arguments)
+        })
+
+    @inspects(AssignmentOperation)
+    def process_assignment_operation(self, node: AssignmentOperation) -> None:
+        if node.intent is AssignmentOperation.DECELERATION:
+            self.process_declaration(node)
+        elif node.intent is AssignmentOperation.REASSIGNMENT:
+            self.process_assignment(node)
+
+    @inspects(MethodCall)
+    def inspect_method_call(self, node: MethodCall) -> None:
+        """
+        The reference indexing for method calls involves 2 separate processes - resolving the method target,
+        and resolving each and every argument
+        """
+        node.arguments = [
+            arg if arg.STATIC else ResolvedReference(self.locals.get(arg)) for arg in node.arguments
+        ]
+
+    def process_declaration(self, node):
+        if node in self.locals:
+            raise Exception('Duplicate decleration')
+        else:
+            print('Setting a local variable')
+            node.target = self.locals.add(node.name)
+
+    def process_assignment(self, node: AssignmentOperation):
+        if node.name.implements(LexicalIdentifier):
+            node.target = self.locals.get(node.name)
+            print('For {}, idx is {}'.format(node.name, self.locals.get(node.name)))
+        else:
+            raise Exception('cannot resolve references yet!')
