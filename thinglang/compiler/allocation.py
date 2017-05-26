1560b5e05d38b0dfebc6bc412a266671e470098b
Yotam Tanay <yotam@paticon.com>
2017-05-26 15:57:25 +0300

    Described a basic linear memory allocator

---
+++ thinglang/compiler/allocation.py
@@ -0,0 +1,18 @@
+class LinearMemoryAllocationLayout(object):
+    def __init__(self, initial=None):
+        initial = initial or {}
+        self.mapping = initial
+        self.next_index = len(initial)
+
+    def add(self, name):
+        index = self.next_index
+        self.mapping[name] = self.next_index
+        self.next_index += 1
+        return index
+
+    def get(self, name):
+        return self.mapping.get(name)
+
+    def __contains__(self, item):
+        return self.mapping.__contains__(item)
+
