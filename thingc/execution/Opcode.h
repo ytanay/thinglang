abe29dda2ea228193d992dd4bb14aa475f717a41
Yotam Tanay <yotam@paticon.com>
2017-05-26 15:54:50 +0300

    Add SET_STATIC opcode to enum

--- thingc/execution/Opcode.h
+++ thingc/execution/Opcode.h
@@ -4,18 +4,19 @@ enum class Opcode {
 	INVALID = 0,
 	NOP = 1,
 	
-	PUSH = 2,
-	PUSH_STATIC = 3,
-	POP = 4,
-	SET = 5,
-	
-	CALL = 6,
-	CALL_METHOD = 7,
-	CALL_INTERNAL = 8,
-	RETURN = 9,
+	PUSH = 2, // pushes a reference into the stack
+	PUSH_STATIC = 3, // pushes static data into the stack
+	POP = 4, // pop anything to void
 
-	PRINT = 10,
+	SET = 5, // pop a reference from the stack and assign it
+	SET_STATIC = 6, // set a reference to static data
 
-    METHOD_END = 11
+	CALL = 7,
+	CALL_METHOD = 8,
+	CALL_INTERNAL = 9,
+	RETURN = 10,
+
+	PRINT = 11,
+
+    METHOD_END = 12
 };
-
