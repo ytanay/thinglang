78a5071ecc9ca51f7a0aca8f431adb55c3140383
Yotam Tanay <yotam@paticon.com>
2017-05-31 19:56:15 +0300

    Centralized location for foundation templates

---
+++ thinglang/foundation/templates.py
@@ -0,0 +1,40 @@
+FOUNDATION_HEADER = """
+#pragma once
+
+#include "../execution/Program.h"
+#include "../containers/ThingInstance.h"
+#include "../utils/formatting.h"
+
+namespace {}Namespace {{
+{}
+}}
+
+""".strip()
+
+FOUNDATION_SOURCE = """
+#include "{name}Instance.h"
+
+namespace {name}Namespace {{
+    const std::vector<InternalMethod> {name}Instance::methods = {{{methods}}};
+}}
+
+""".strip()
+
+
+FOUNDATION_ENUM = """
+#pragma once
+
+enum class InternalTypes {{
+{}
+}};
+""".strip()
+
+
+FOUNDATION_VIRTUALS = """
+virtual std::string text() const override {{
+    return to_string({first_member});
+}}
+virtual void call_method(unsigned int target) override {{
+    internals[target]();
+}}
+"""
