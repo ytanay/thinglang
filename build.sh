2140296c8e04042a561c25f62e428f1a44cbfe2f
Yotam Tanay <yotam@paticon.com>
2017-06-02 15:48:49 +0300

    Add virtual env generation and pytest to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+python3.6 -m venv thinglang-env
+source thinglang-env/bin/activate
+pip install -r requirements.txt
+
 mkdir /app-src/build
 cd /app-src/build
 cmake ..
 make
 /app-src/build/thinglang --build-only
+
+pytest tests/integration
