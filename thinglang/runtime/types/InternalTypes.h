75053597284c72578e1ffceb8cff1c92b40a412c
Yotam Tanay <yotam@paticon.com>
2017-06-08 23:08:43 +0300

    Improved code generation for enum types

--- thinglang/runtime/types/InternalTypes.h
+++ thinglang/runtime/types/InternalTypes.h
@@ -1,5 +1,30 @@
+/**
+    InternalTypes.h
+    Auto-generated code - do not modify.
+    thinglang C++ transpiler, 0.0.0
+**/
+
 #pragma once
 
+#include <string>
+
 enum class InternalTypes {
-NONE = 0, TEXT = -1, NUMBER = -2
+    NONE = 0,
+    TEXT = -1,
+    NUMBER = -2
 };
+
+inline std::string describe(InternalTypes val){
+     switch (val){
+        
+    case InternalTypes::NONE:
+        return "NONE";
+
+    case InternalTypes::TEXT:
+        return "TEXT";
+
+    case InternalTypes::NUMBER:
+        return "NUMBER";
+    }
+
+}
