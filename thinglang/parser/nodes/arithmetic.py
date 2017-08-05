0686f7e66d9116b0b2adf748b3cd866a77aaab76
Yotam Tanay <yotam.tanay@gmail.com>
2017-08-05 13:43:23 +0300

    Cleaned up old ArithmeticOperation mess

--- thinglang/parser/nodes/arithmetic.py
+++ thinglang/parser/nodes/arithmetic.py
@@ -26,29 +26,14 @@ class ArithmeticOperation(BaseNode, ValueType, ReplaceableArguments):
         self.arguments = [slice[0], slice[2]]
         self.operator = type(slice[1])
 
-    def evaluate(self, resolver):
-        return self.OPERATIONS[self.operator](self[0].evaluate(resolver), self[1].evaluate(resolver))
+    def evaluate(self):
+        return self.OPERATIONS[self.operator](self[0].evaluate(), self[1].evaluate())
 
     def describe(self):
         return '|{} {} {}|'.format(self[0], self.operator, self[1])
 
-    def __getitem__(self, item):
-        return self.arguments[item]
-
-    def references(self):
-        return self.arguments
-
     def transpile(self):
         return '{} {} {}'.format(self.arguments[0].transpile(), REVERSE_OPERATORS[self.operator], self.arguments[1].transpile())
 
-    def compile(self, context: CompilationContext, captured=False): # TODO: duplicate of actual call
-        context.push_ref(self.arguments[1])
-        context.push_ref(self.arguments[0])
-        type_id = self.arguments[0].type_id()
-        context.append(OpcodeCallInternal(Foundation().INTERNAL_TYPE_ORDERING[type_id], Foundation().type(type_id).index(self.operator.transpile())))
-        if self.parent is not None and not captured:
-            context.append(BytecodeSymbols.pop())  # TODO: Dead branch?
-
-    def type_id(self):
-        assert all(x.type_id() == self.arguments[0].type_id() for x in self.arguments)
-        return self.arguments[0].type_id()
+    def __getitem__(self, item):
+        return self.arguments[item]
