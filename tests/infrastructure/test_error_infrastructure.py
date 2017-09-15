e54724584287ffb19e0fa3769da11f1b9e200ff5
Yotam Tanay <yotam.tanay@gmail.com>
2017-09-15 19:36:31 +0300

    Removed redundant test

--- tests/infrastructure/test_error_infrastructure.py
+++
@@ -1,7 +0,0 @@
-from thinglang.lexer.tokens.base import LexicalIdentifier
-from thinglang.parser.errors import UnresolvedReference
-
-
-def test_error_infrastructure():
-    assert UnresolvedReference(LexicalIdentifier("a")) == UnresolvedReference(LexicalIdentifier("a"))
-    assert UnresolvedReference(LexicalIdentifier("a")) != UnresolvedReference(LexicalIdentifier("b"))
