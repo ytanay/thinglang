b52f3c67de94da2d575bdb7561df713a621a44a0
Yotam Tanay <yotam.tanay@gmail.com>
2017-09-16 11:00:42 +0300

    Added additional parser error tests

--- tests/parser/test_parser_errors.py
+++ tests/parser/test_parser_errors.py
@@ -9,20 +9,23 @@ INVALID_SYNTAX_EXAMPLES = [
     'does a returns b with number a',
     'a b',
     'f()',
-    '2 eq'
+    '2 eq',
+    'for',
+    'number if = 1'
 ]
 
 VALID_SYNTAX_EXAMPLES = [
     '2 + 2',
     'does walk',
-    'a.b()'
+    'a.b()',
+    'if a'
 ]
 
 
 @pytest.mark.parametrize('source', INVALID_SYNTAX_EXAMPLES)
 def test_invalid_syntax_examples(source):
     with pytest.raises(VectorReductionError):
-        print(parse_local(source))
+        parse_local(source)
 
 
 @pytest.mark.parametrize('source', VALID_SYNTAX_EXAMPLES)
