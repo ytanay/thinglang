ee4c22a24a8dcf693dbcacc257d09c754c5d3979
Yotam Tanay <yotam.tanay@gmail.com>
2017-10-11 23:44:18 +0300

    Add generated code for file type

---
+++ thinglang/runtime/types/core/FileType.cpp
@@ -0,0 +1,80 @@
+/**
+    FileType.cpp
+    Auto-generated code - do not modify.
+    thinglang C++ transpiler, 0.0.0
+**/
+
+
+#include "../InternalTypes.h"
+
+/**
+Methods of FileType
+**/
+
+
+Thing FileType::__constructor__() {
+		auto file_path = Program::argument<TextInstance>();
+		auto self = std::static_pointer_cast<FileInstance>(Thing(new FileInstance()));
+
+		self->file_path = file_path->text();
+        return self;
+    }
+
+
+Thing FileType::open() {
+		auto mode = Program::argument<TextInstance>();
+		auto self = Program::argument<FileInstance>();
+
+		
+        if(mode->text() == "r") {
+			self->file.open(self->file_path, std::ios::in);
+        }
+
+		
+        else if(mode->text() == "w") {
+			self->file.open(self->file_path, std::ios::out);
+        }
+
+        return nullptr;
+    }
+
+
+Thing FileType::close() {
+		auto self = Program::argument<FileInstance>();
+
+		self->file.close();
+        return nullptr;
+    }
+
+
+Thing FileType::write() {
+		auto contents = Program::argument<TextInstance>();
+		auto self = Program::argument<FileInstance>();
+
+		self->file << contents->text();
+        return nullptr;
+    }
+
+
+Thing FileType::read() {
+		auto self = Program::argument<FileInstance>();
+
+		std::stringstream sstr;
+		sstr << self->file.rdbuf();
+		return Thing(new TextInstance(sstr.str()));
+        return nullptr;
+    }
+
+
+/**
+Mixins of FileInstance
+**/
+
+std::string FileInstance::text() {
+    return to_string(file_path);
+}
+
+bool FileInstance::boolean() {
+    return to_boolean(file_path);
+}
+
---
+++ thinglang/runtime/types/core/FileType.h
@@ -0,0 +1,49 @@
+/**
+    FileType.h
+    Auto-generated code - do not modify.
+    thinglang C++ transpiler, 0.0.0
+**/
+
+#pragma once
+
+#include "../../utils/TypeNames.h"
+#include "../../execution/Globals.h"
+#include "../../utils/Formatting.h"
+#include "../infrastructure/ThingType.h"
+#include "../infrastructure/ThingInstance.h"
+#include "../../execution/Program.h"
+
+
+
+
+class FileInstance : public BaseThingInstance {
+    
+    public:
+    explicit FileInstance() = default; // empty constructor
+    
+    explicit FileInstance(std::string file_path, std::fstream file) : file_path(file_path) {}; // value constructor
+    
+    /** Mixins **/
+    
+    virtual std::string text() override;
+    virtual bool boolean() override;
+
+    
+    /** Members **/
+    std::string file_path;
+std::fstream file;
+};
+
+
+class FileType : public ThingTypeInternal {
+    
+    public:
+    FileType() : ThingTypeInternal({ &__constructor__, &open, &close, &write, &read }) {}; // constructor
+ 
+	static Thing __constructor__();
+	static Thing open();
+	static Thing close();
+	static Thing write();
+	static Thing read();
+    
+};
