915ee5563f2d02edbd3181b262cd1a01f667c45c
Yotam Tanay <yotam.tanay@gmail.com>
2017-11-04 13:47:14 +0200

    Simple unit test for mark-sweep GC

---
+++ tests/runtime/gc/test_gc.cpp
@@ -0,0 +1,51 @@
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.h"
+#include "../../../thinglang/runtime/types/core/ListType.h"
+#include "../../../thinglang/runtime/types/core/TextType.h"
+
+
+TEST_CASE( "Full flow GC test", "[gc]" ) {
+
+
+    REQUIRE(Program::object_count() == 0);
+
+    Thing text1 = Program::create<TextInstance>("Hello World!");
+    Thing text2 = Program::create<TextInstance>("Goodbye World!");
+    std::vector<Thing> things = {text1, text2};
+
+    Thing list1 = Program::create<ListInstance>(things);
+
+    REQUIRE(Program::object_count() == 3);
+
+    Program::create_frame(1);
+    Program::frame()[0] = list1;
+
+    Program::mark();
+    Program::sweep();
+
+    REQUIRE(Program::object_count() == 3);
+
+    Program::push(list1);
+    ListType::pop();
+
+    Program::mark();
+    Program::sweep();
+
+    REQUIRE(Program::object_count() == 2);
+
+    Program::push(list1);
+    ListType::pop();
+
+    Program::mark();
+    Program::sweep();
+
+    REQUIRE(Program::object_count() == 1);
+
+
+
+
+
+
+
+
+}
