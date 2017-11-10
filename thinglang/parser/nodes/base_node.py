36914e5dcd0663361a75c1944fc64968a4b1f451
Yotam Tanay <yotam.tanay@gmail.com>
2017-11-10 15:21:59 +0200

    Split node registration into mixin class

--- thinglang/parser/nodes/base_node.py
+++ thinglang/parser/nodes/base_node.py
@@ -2,21 +2,11 @@ import itertools
 
 from thinglang.foundation import templates
 from thinglang.utils import collection_utils
+from thinglang.utils.mixins import ParsingMixin
 from thinglang.utils.source_context import SourceReference
 
 
-class NodeRegistration(type):
-    def __new__(mcs, name, bases, dct):
-        mcs = super(NodeRegistration, mcs).__new__(mcs, name, bases, dct)
-
-        mcs.RULES = sorted((value.__func__.parser_rule
-                            for field, value in dct.items()
-                            if hasattr(value, '__func__')
-                            and hasattr(value.__func__, 'parser_rule')), key=lambda x: x.index)
-        return mcs
-
-
-class BaseNode(object, metaclass=NodeRegistration):
+class BaseNode(ParsingMixin):
     STATIC = False
 
     def __init__(self, tokens):
@@ -76,16 +66,6 @@ class BaseNode(object, metaclass=NodeRegistration):
     def deriving_from(self, node):
         self.source_ref = node.source_ref
         return self
-
-    @classmethod
-    def propose_replacement(cls, tokens):
-        for rule in cls.RULES:
-            result = rule.matches(tokens)
-
-            if result:
-                return result
-
-        return False
 
     @property
     def container_name(self):
