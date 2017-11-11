8aba7bf5c18a435628d27c0d505d043adfc5e40e
Yotam Tanay <yotam.tanay@gmail.com>
2017-11-11 19:43:43 +0200

    Attempt to correctly infer inline list type

--- thinglang/parser/values/inline_list.py
+++ thinglang/parser/values/inline_list.py
@@ -1,6 +1,6 @@
 from thinglang.compiler.buffer import CompilationBuffer
 from thinglang.compiler.context import CompilationContext
-from thinglang.lexer.values.identifier import Identifier
+from thinglang.lexer.values.identifier import Identifier, GenericIdentifier
 from thinglang.parser.common.list_type import ListInitialization
 from thinglang.parser.definitions.argument_list import ArgumentList
 from thinglang.parser.values.access import Access
@@ -9,13 +9,24 @@ from thinglang.parser.values.method_call import MethodCall
 
 class InlineList(ListInitialization):
 
+    def __init__(self, values):
+        super().__init__(values)
+        self.inferred_type = None
+
     def replace_argument(self, idx, replacement):
         self.arguments[idx] = replacement
 
     def compile(self, context: CompilationBuffer):
-        last_call = MethodCall(Access([Identifier("list"), Identifier.constructor()])).deriving_from(self)
+        if not self.values:
+            return
 
-        for value in self:
+        buffer = context.optional()
+        ref = self[0].compile(buffer)  # TODO: remove unnecessary recompilation of first element (used to infer type)
+
+        list_type = GenericIdentifier(Identifier('list'), (ref.type,))
+        last_call = MethodCall(Access([list_type, Identifier.constructor()])).deriving_from(self)
+
+        for value in self:  # TODO: validate list is homogeneous, and descend to lowest common type
             last_call = MethodCall(Access([last_call, Identifier("append")]), ArgumentList([value])).deriving_from(self)
 
         return last_call.compile(context)
