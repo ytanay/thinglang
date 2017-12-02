0af3b589c6c271d07ad4e204fa41aa0fed167a94
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-02 23:44:32 +0200

    Add explicit parsing rule for cast operations

--- thinglang/parser/constructs/cast_operation.py
+++ thinglang/parser/constructs/cast_operation.py
@@ -1,9 +1,13 @@
+from thinglang.lexer.operators.casts import LexicalCast
 from thinglang.lexer.values.identifier import Identifier
+from thinglang.parser.nodes import BaseNode
+from thinglang.parser.rule import ParserRule
 from thinglang.parser.values.access import Access
 from thinglang.parser.values.method_call import MethodCall
+from thinglang.utils.type_descriptors import ValueType
 
 
-class CastOperation(object):
+class CastOperation(BaseNode):
     """
     Explicitly cast from one type to another
     Expects a conversion method on the source class
@@ -12,3 +16,8 @@ class CastOperation(object):
     @staticmethod
     def create(source: Identifier, destination: Identifier) -> MethodCall:
         return MethodCall(Access([source, Identifier('convert_') + destination]), MethodCall.STACK_ARGS)
+
+    @staticmethod
+    @ParserRule.mark
+    def parse_inline_cast_op(value: ValueType, _: LexicalCast, target_type: Identifier):
+        return MethodCall(Access([value, Identifier('convert_') + target_type]), [])
