92530bac57d1c7d4986d3e9ae02200995b8b1195
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-02 23:43:41 +0200

    Add iteration loop construct

---
+++ thinglang/parser/blocks/iteration_loop.py
@@ -0,0 +1,35 @@
+from thinglang.compiler.buffer import CompilationBuffer
+from thinglang.lexer.blocks.loops import LexicalRepeatFor
+from thinglang.lexer.operators.membership import LexicalIn
+from thinglang.lexer.values.identifier import Identifier
+from thinglang.parser.blocks.loop import Loop
+from thinglang.parser.rule import ParserRule
+from thinglang.parser.statements.assignment_operation import AssignmentOperation
+from thinglang.parser.values.access import Access
+from thinglang.parser.values.method_call import MethodCall
+from thinglang.utils.type_descriptors import ValueType
+
+
+class IterationLoop(Loop):
+
+    def __init__(self, target: Identifier, target_type: Identifier, collection: ValueType):
+        self.target, self.target_type, self.collection = target, target_type, collection
+        self.iterator = MethodCall(Access([collection, "iterator"]))
+        super().__init__(MethodCall(Access([self.iterator, 'has_next']), stack_target=True))
+
+    def finalize(self):
+        self.children.insert(0, AssignmentOperation(
+            AssignmentOperation.REASSIGNMENT,
+            self.target,
+            MethodCall(Access([self.iterator, 'next']), stack_target=True),
+            self.target_type
+        ).deriving_from(self))
+
+    def compile(self, context: CompilationBuffer):
+        self.iterator.compile(context)  # Create the iterator, pushing onto the stack
+        super().compile(context)  # Compile the body of the loop
+
+    @staticmethod
+    @ParserRule.mark
+    def parse_iterative_loop(_1: LexicalRepeatFor, target_type: Identifier, target: Identifier, _2: LexicalIn, collection: ValueType):
+        return IterationLoop(target, target_type, collection)
