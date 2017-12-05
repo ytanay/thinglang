27223a9e832ad7b8f41b048ed0cda2dfbc72b0b8
Yotam Tanay <yotam.tanay@forter.com>
2017-12-05 21:13:31 +0200

    Explicit error for invalid indexed access

--- thinglang/parser/errors.py
+++ thinglang/parser/errors.py
@@ -6,3 +6,12 @@ class VectorReductionError(ThinglangException):
     A generic parser error, thrown when a vector of lexical tokens could not be
     entirely reduced - that is to say, invalid syntax was supplied by the user.
     """
+
+
+class InvalidIndexedAccess(ThinglangException):
+    """
+    Thrown when an invalid indexed access has been made.
+    Examples:
+        a[]
+        a[1, 2, 3]
+    """
