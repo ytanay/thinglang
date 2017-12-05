c45aa3998b97e982d80fc64445abb45532b863a2
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-05 21:33:47 +0200

    Finalize parsing rules for UniversalAccess

--- thinglang/parser/values/access_universal.py
+++ thinglang/parser/values/access_universal.py
@@ -22,8 +22,19 @@ class UniversalAccess(BaseNode):
         return Access(root.target + [extension])
 
     @staticmethod
-    @ParserRule.predicate(lambda left, _, right: not ParserRule.is_instance(left, 'BracketVector'))
+    @ParserRule.predicate(lambda tokens, index: index == 0)
+    def parse_access_root(left: 'BracketVector', _: LexicalAccess, right: Identifier):
+        """
+        Only parse [1, 2, 3].property if the BracketVector is the first token in the stream
+        """
+        return Access([left, right])
+
+    @staticmethod
+    @ParserRule.predicate(lambda tokens, index: not ParserRule.is_instance(tokens[0], 'BracketVector'))
     def parse_access_predicated(left: ValueType, _: LexicalAccess, right: Identifier):
+        """
+        Parse the all other named access constructions
+        """
         return Access([left, right])
 
     @staticmethod
@@ -33,8 +44,3 @@ class UniversalAccess(BaseNode):
             raise InvalidIndexedAccess(idx)
 
         return IndexedAccess(target, idx[0])
-
-    @staticmethod
-    @ParserRule.mark
-    def parse_access_root(left: ValueType, _: LexicalAccess, right: Identifier):
-        return Access([left, right])
