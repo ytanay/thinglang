c3d8df7559ecb163d0bc637b3d6c20ce3274e342
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-14 01:20:36 +0200

    Pass down SourceLine objects to SourceRefs

--- thinglang/utils/source_context.py
+++ thinglang/utils/source_context.py
@@ -30,12 +30,12 @@ class SourceLine(object):
     Wraps a single line of code
     """
 
-    def __init__(self, source, line_number, filename):
+    def __init__(self, source: str, line_number: int, filename: str):
         self.source, self.line_number, self.filename = source.replace(' ' * 4, '\t').rstrip(), line_number, filename
 
     def __iter__(self):
         for column_number, char in enumerate(self.source):
-            yield char, SourceReference(self.filename, self.line_number, column_number, column_number + 1)
+            yield char, SourceReference(self, column_number, column_number + 1)
 
     @property
     def empty(self):
@@ -51,30 +51,30 @@ class SourceReference(object):
     Refers to a slice of source code, from which a lexical token (or AST node) was derived.
     """
 
-    def __init__(self, filename, line_number, column_start, column_end):
-        self.filename, self.line_number, self.column_start, self.column_end = filename, line_number, column_start, column_end
+    def __init__(self, source_line: SourceLine, column_start, column_end):
+        self.source_line, self.column_start, self.column_end = source_line, column_start, column_end
 
     def __repr__(self):
-        return '{}:{}:{}-{}'.format(self.filename, self.line_number, self.column_start, self.column_end)
+        return f'{self.source_line.source} (at {self.filename}:{self.line_number}:{self.column_start}-{self.column_end})'
 
     def __sub__(self, other):
         assert self.filename == other.filename and self.line_number == other.line_number and self.column_start >= other.column_start
-        return SourceReference(self.filename, self.line_number, other.column_start, self.column_start)
+        return SourceReference(self.source_line, other.column_start, self.column_start)
 
     def __add__(self, other):
         assert isinstance(other, int)
-        return SourceReference(self.filename, self.line_number, self.column_start, self.column_end + other)
+        return SourceReference(self.source_line, self.column_start, self.column_end + other)
 
     def serialize(self):
         return struct.pack('<i', self.line_number)
 
     @classmethod
     def invalid(cls):
-        return cls('<invalid>', -1, -1, -1)
+        return cls(SourceLine('', -1, '<invalid>'), -1, -1)
 
     @classmethod
     def generated(cls, reason):
-        return cls('<generated:{}>'.format(reason), -1, -1, -1)
+        return cls(SourceLine('', -1, '<generated:{}>'.format(reason)), -1, -1)
 
     @classmethod
     def combine(cls, tokens):
@@ -84,4 +84,12 @@ class SourceReference(object):
             return None
 
         assert len(set(x.filename for x in refs)) == len(set(x.line_number for x in refs)) == 1
-        return cls(refs[0].filename, refs[0].line_number, min(x.column_start for x in refs), max(x.column_end for x in refs))
+        return cls(refs[0].source_line, min(x.column_start for x in refs), max(x.column_end for x in refs))
+
+    @property
+    def filename(self):
+        return self.source_line.filename
+
+    @property
+    def line_number(self):
+        return self.source_line.line_number
