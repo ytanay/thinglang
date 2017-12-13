d9953c8dff2bbf17a67fb8982a213931c9a61482
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-14 01:11:40 +0200

    Add setup.py for local development

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='thinglang',
+    version='0.0.0',
+
+    description='Yet another general purpose programming language',
+    long_description=long_description,
+
+    url='https://github.com/ytanay/thinglang',
+
+    author='Yotam Tanay',
+    author_email='yotam@yotamtanay.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+)
