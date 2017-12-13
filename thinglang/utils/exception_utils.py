72404a01f3fc860743a71434dd794cd1ea3f3b8d
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-14 01:33:39 +0200

    Additional fixes for error message generation

--- thinglang/utils/exception_utils.py
+++ thinglang/utils/exception_utils.py
@@ -3,10 +3,4 @@ class ThinglangException(Exception):
     The base thinglang exception class
     """
 
-    def __init__(self, *args):
-        self.args = args
-        super(ThinglangException, self).__init__(self.message)
-
-    @property
-    def message(self):
-        return f'{type(self).__name__}: {self.args}'
+    pass
