71f6a498178fcf391f7942a11b6af170c43d8f0d
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-14 01:21:35 +0200

    Normalize exception message generation

--- thinglang/parser/errors.py
+++ thinglang/parser/errors.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from thinglang.utils.exception_utils import ThinglangException
 
 
@@ -6,6 +8,13 @@ class VectorReductionError(ThinglangException):
     A generic parser error, thrown when a vector of lexical tokens could not be
     entirely reduced - that is to say, invalid syntax was supplied by the user.
     """
+
+    def __init__(self, reason: str, tokens: List):
+        self.reason, self.tokens = reason, tokens
+        super().__init__(str(self))
+
+    def __str__(self):
+        return f'{self.reason}: {self.tokens[0].source_ref}'
 
 
 class InvalidIndexedAccess(ThinglangException):
