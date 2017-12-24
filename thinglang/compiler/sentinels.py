db24629b7cc34f9a137c6bc5569dc7a39245fa52
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-24 22:31:09 +0200

    Add serialization sentintels for import table

--- thinglang/compiler/sentinels.py
+++ thinglang/compiler/sentinels.py
@@ -2,6 +2,18 @@ from thinglang.compiler.opcodes import MEMBERS, METHODS, FRAME_SIZE, ARGUMENTS
 from thinglang.compiler.opcodes import Opcode
 
 
+class SentinelImportTableEntry(Opcode):
+    """
+    Signifies an import table entry
+    """
+
+
+class SentinelImportTableEnd(Opcode):
+    """
+    Signifies the end of the import table
+    """
+
+
 class SentinelThingDefinition(Opcode):
     """
     Signifies the start of thing definition
