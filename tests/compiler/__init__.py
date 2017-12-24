dfbb93a132d9165e53478b579db92fa0b4cf02ce
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-24 18:57:14 +0200

    Update compiler test infrastructure

--- tests/compiler/__init__.py
+++ tests/compiler/__init__.py
@@ -1,13 +1,17 @@
+import itertools
+
 from thinglang import pipeline
 from thinglang.compiler.opcodes import OpcodeCallInternal
 from thinglang.lexer.values.identifier import Identifier
 from thinglang.symbols.symbol_mapper import SymbolMapper
 from thinglang.utils.source_context import SourceContext
 
-SELF_ID, A_ID, B_ID, INST_ID, LST_ID, IMPLICIT_ITERATOR_ID, IMPLICIT_ITERATION_ID = 0, 1, 2, 3, 4, 5, 6
+SELF_ID, A_ID, B_ID, INST_ID, LST_ID, A_INST, B_INST, C_INST, IMPLICIT_ITERATOR_ID, IMPLICIT_ITERATION_ID = range(10)
 VAL1_ID, VAL2_ID, INNER_ID = 0, 1, 2
 INNER1_ID, INNER2_ID = 0, 1
-STATIC_START = 5
+STATIC_START = 6
+
+PROGRAM, CONTAINER, A_THING, B_THING, C_THING = range(5)
 
 TEMPLATE = """
 thing Program
@@ -20,6 +24,9 @@ thing Program
         number b = 0
         Program inst = Program()
         list<number> lst = [0, 1, 2]
+        A a_inst = A()
+        B b_inst = B(42)
+        C c_inst = C()
         
         {}
     
@@ -29,21 +36,38 @@ thing Program
 
 thing Container
     has number inner1
-    has number inner2    
+    has number inner2
+    
+thing A
+    has number a1
+
+thing B extends A
+    has number b1
+    
+    setup with number b1
+        self.b1 = b1
+        
+thing C extends B
+    has number c1
+    
+    
+        
 """
 
 
-def compile_base(code):
+def compile_base(code='', thing_id=0, method_id=0):
     context = pipeline.compile(SourceContext.wrap(TEMPLATE.format(code)))
-    entry = context.methods[(0, 0)]
+    entry = context.methods[(thing_id, method_id)]
     return entry[1].instructions
 
 
-TRIM_START = len(compile_base('')) - 1
+TRIM_START = len(compile_base()) - 1
 
 
-def compile_local(code):
-    return compile_base(code)[TRIM_START:-1]  # Pop off boilerplate and the return instruction
+def compile_snippet(code):
+    instructions = compile_base(code)[TRIM_START:-1]  # Pop off boilerplate and the return instruction
+    print('Bytecode result: {}'.format(instructions))
+    return instructions
 
 
 def internal_call(target):
