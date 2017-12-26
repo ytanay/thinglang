bd31d53fe1dd3d0648a6cd791245f6532df4f4bd
Yotam Tanay <yotam.tanay@gmail.com>
2017-12-26 10:48:43 +0200

    Fix assignment test to correctly test implicit casts

--- tests/compiler/test_assignment_compilation.py
+++ tests/compiler/test_assignment_compilation.py
@@ -1,12 +1,12 @@
 from tests.compiler import compile_snippet, A_ID, B_ID, SELF_ID, VAL1_ID, VAL2_ID, INST_ID, INNER_ID, INNER1_ID, \
-    INNER2_ID, STATIC_START
+    INNER2_ID, STATIC_START, internal_call
 from thinglang.compiler.opcodes import OpcodeAssignStatic, OpcodeAssignLocal, OpcodePushMember, OpcodePopLocal, \
-    OpcodeDereference
+    OpcodeDereference, OpcodePushStatic
 
 
 def test_static_to_local():
     assert compile_snippet('a = 5') == [OpcodeAssignStatic(A_ID, STATIC_START)]
-    assert compile_snippet('a = "hello"') == [OpcodeAssignStatic(A_ID, STATIC_START)]
+    assert compile_snippet('a = "hello"') == [OpcodePushStatic(STATIC_START), internal_call('text.as number'), OpcodePopLocal(A_ID)]
 
 
 def test_local_to_local():
