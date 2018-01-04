89980675458063e6bb941d7a61a650a31d1321f1
Yotam Tanay <yotam.tanay@gmail.com>
2018-01-04 22:40:54 +0200

    Add tests for assignment to targets containing an indexed access

--- tests/compiler/test_assignment_compilation.py
+++ tests/compiler/test_assignment_compilation.py
@@ -1,7 +1,7 @@
 from tests.compiler import compile_snippet, A_ID, B_ID, SELF_ID, VAL1_ID, VAL2_ID, INST_ID, INNER_ID, INNER1_ID, \
-    INNER2_ID, STATIC_START, internal_call
+    INNER2_ID, STATIC_START, internal_call, LST_ID, CONTAINER
 from thinglang.compiler.opcodes import OpcodeAssignStatic, OpcodeAssignLocal, OpcodePushMember, OpcodePopLocal, \
-    OpcodeDereference, OpcodePushStatic
+    OpcodeDereference, OpcodePushStatic, OpcodePushLocal, OpcodeCallStatic, OpcodePopDereferenced
 
 
 def test_static_to_local():
@@ -33,3 +33,20 @@ def test_inner_member_to_local():
     assert compile_snippet('b = inst.inner.inner2') == [OpcodePushMember(INST_ID, INNER_ID), OpcodeDereference(INNER2_ID), OpcodePopLocal(B_ID)]
 
 
+def test_static_to_indexed():
+    assert compile_snippet('lst[0] = Container()') == [
+        OpcodePushStatic(STATIC_START),  # Push index 0
+        OpcodeCallStatic(CONTAINER, 0),  # Push constant 5
+        OpcodePushLocal(LST_ID),  # Push the list
+        internal_call('list.set')
+    ]
+
+
+def test_static_to_member_of_indexed():
+    assert compile_snippet('lst[0].inner1 = 5') == [
+        OpcodePushStatic(STATIC_START),  # Push index 0
+        OpcodePushStatic(STATIC_START + 1),  # Push constant 5
+        OpcodePushLocal(LST_ID),  # Push the list
+        internal_call('list.get'),
+        OpcodePopDereferenced(INNER1_ID)
+    ]
